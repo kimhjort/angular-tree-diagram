1ac04d3261614efddec1dbb398788f6c8d231ed0
Kim Hjort Madsen <khm@inLogic.dk>
2022-05-17 15:13:05 +0200

    Updated the lib Updated gitignore Exported treediagram to public api

--- projects/ng-tree-diagram/src/ng-tree-diagram.ts
+++ projects/ng-tree-diagram/src/ng-tree-diagram.ts
@@ -6,3 +6,4 @@ export * from './lib/tree.module';
 export * from './lib/services/nodes-list.service';
 export * from './lib/node';
 export * from './lib/classes';
+export * from './lib/tree.component';
